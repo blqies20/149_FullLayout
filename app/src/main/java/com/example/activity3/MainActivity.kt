09a8b742df5f21b58d7473a5888ae7b2d578af0c
ACER <ACER@DESKTOP-QH5VR0V>
2023-10-19 12:57:58 +0700

    Box keterangan

--- app/src/main/java/com/example/activity3/MainActivity.kt
+++ app/src/main/java/com/example/activity3/MainActivity.kt
@@ -6,9 +6,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -27,6 +30,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -58,8 +62,8 @@ fun HomeScreen(){
         horizontalAlignment = Alignment.CenterHorizontally
     ){
         Text(
-            text = "cuaca",
-            fontSize = 21.sp,
+            text = "WEATHER APP",
+            fontSize = 30.sp,
             fontWeight = FontWeight.Bold,
         )
 
@@ -70,6 +74,10 @@ fun HomeScreen(){
         Spacer(modifier = Modifier.padding(10.dp))
 
         Loc()
+
+        Spacer(modifier = Modifier.padding(10.dp))
+
+        BoxKeterangan()
     }
 }
 
@@ -83,10 +91,13 @@ fun BoxLayar(){
         Column(
             horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()
         ) {
-            Image(painter = painterResource(id = R.drawable.img), contentDescription = "cuaca",
-                modifier = Modifier.fillMaxWidth().padding(20.dp))
+            Image(painter = painterResource(id = R.drawable.img), contentDescription = "",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp)
+                    .size(150.dp))
 
-            Text(text = "hujan", fontSize = 30.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(13.dp))
+            Text(text = "Drizzly", fontSize = 30.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(13.dp))
         }
     }
 
@@ -94,7 +105,49 @@ fun BoxLayar(){
 
 @Composable
 fun Loc(){
-    Text(text = "19 C", fontSize = 60.sp, fontWeight = FontWeight.Bold)
+    Text(text = "19°C", fontSize = 60.sp, fontWeight = FontWeight.Bold)
+
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(painter = painterResource(id = R.drawable.location), contentDescription = "", modifier = Modifier.size(35.dp))
+
+        Spacer(modifier = Modifier.padding(3.dp))
+
+        Text(text = "Yogyakarta", fontSize = 40.sp, fontWeight = FontWeight.Bold)
+    }
+
+    Spacer(modifier = Modifier.padding(13.dp))
+
+    Text(text = "Kasihan, Kabupaten Bantul, Daerah Istimewa Yogyakarta", textAlign = TextAlign.Center, fontSize = 20.sp, color = Color.Gray)
+}
+
+@Composable
+fun BoxKeterangan(){
+    Card(colors = CardDefaults.cardColors(Color.LightGray)
+    ) {
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround, verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxHeight(), verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "Humidity", fontSize = 20.sp, color = Color.Black, modifier = Modifier.padding(5.dp))
+                Text(text = "90%", fontSize = 25.sp, fontWeight = FontWeight.Bold, color = Color.Black)
+
+                Spacer(modifier = Modifier.padding(10.dp))
+
+                Text(text = "Sunrise", fontSize = 20.sp, color = Color.Black, modifier = Modifier.padding(5.dp))
+                Text(text = "05.00 AM", fontSize = 25.sp, fontWeight = FontWeight.Bold, color = Color.Black)
+            }
+            Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxHeight(), verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "UV Index", fontSize = 20.sp, color = Color.Black, modifier = Modifier.padding(5.dp))
+                Text(text = "7/10", fontSize = 25.sp, fontWeight = FontWeight.Bold)
+
+                Spacer(modifier = Modifier.padding(10.dp))
+
+                Text(text = "Sunset", fontSize = 20.sp, color = Color.Black, modifier = Modifier.padding(5.dp))
+                Text(text = "05.53 PM", fontSize = 25.sp, fontWeight = FontWeight.Bold)
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
